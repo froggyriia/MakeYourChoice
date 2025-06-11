a9096349525a479080061db3ae1d986731425e5c
Daria <dariapotapova@yandex.ru>
2025-06-11 16:17:40 +0300

    database is done. it can be taken from supabase_client.py DATABASE

--- supabase_client.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # Загружает переменные из .env
-
-supabase_url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
-
-print(supabase_url)  # Проверка загрузки
