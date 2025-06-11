b43a7a05c9ff653e540b433b2bc059334cb8b72b
Daria <dariapotapova@yandex.ru>
2025-06-11 15:57:07 +0300

    .env

--- supabase_client.py
+++ supabase_client.py
@@ -1,18 +1,9 @@
-import os
-from supabase import create_client, Client
 from dotenv import load_dotenv
+import os
 
-load_dotenv()
+load_dotenv()  # Загружает переменные из .env
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+supabase_url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
 
-supabase: Client = create_client(url, key)
-
-# Пример запроса
-def get_users():
-    response = supabase.table("users").select("*").execute()
-    print(response.data)
-
-if __name__ == "__main__":
-    get_users()
+print(supabase_url)  # Проверка загрузки
