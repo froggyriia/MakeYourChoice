a3411c7bd40f03ac4fac2be2b85b8f5326bc286f
Daria <dariapotapova@yandex.ru>
2025-06-14 13:42:42 +0300

    delete unnecessary stuff

--- Backend/main.py
+++
@@ -1,4 +0,0 @@
-from django.db import connection
-
-connection.ensure_connection()
-print("Подключение к Supabase успешно!")
--- db/supabase_client.py
+++
@@ -1,9 +0,0 @@
-import os
-from supabase import create_client, Client
-from dotenv import load_dotenv
-
-load_dotenv()
-
-url: str = os.getenv("SUPABASE_URL")
-key: str = os.getenv("SUPABASE_KEY")
-DATABASE: Client = create_client(url, key)
