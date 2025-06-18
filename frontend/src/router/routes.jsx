0a8185879eb2f86383000db0f32d8a5d29f4c465
unknown <a.narimov@innopolis.university>
2025-06-18 14:49:52 +0300

    Arif

--- frontend/src/router/routes.jsx
+++ frontend/src/router/routes.jsx
@@ -1,30 +1,24 @@
-//routes.jsx
-
 import LoginPage from '../pages/LoginPage.jsx';
 import CataloguePage from '../pages/CataloguePage.jsx';
 import { Navigate } from 'react-router-dom';
 import ProtectedRoute from "../components/ProtectedRoute.jsx";
 import CourseFormPage from '../pages/CourseFormpage.jsx';
 
-
-// Определяем маршруты приложения
 const routes = [
-    // Страница логина, открыта всем
     { path: '/', element: <LoginPage /> },
-    // Каталог курсов — защищенный маршрут, доступен любому авторизованному пользователю
     {
         path: '/catalogue',
         element: <ProtectedRoute element={<CataloguePage />} />
     },
-    // Админская страница (можно переиспользовать CataloguePage), доступна только админам
     {
         path: '/admin',
         element: <ProtectedRoute element={<CataloguePage />} requireAdmin={true} />
     },
-    // Все остальные пути редиректим на логин
-    { path: '*', element: <Navigate to="/" /> },
-
-    {path: '/form', element: <ProtectedRoute element={<CourseFormPage />} />}
+    {
+        path: '/form',
+        element: <ProtectedRoute element={<CourseFormPage />} requireAdmin={false} />
+    },
+    { path: '*', element: <Navigate to="/" replace /> }
 ];
 
 export default routes;
